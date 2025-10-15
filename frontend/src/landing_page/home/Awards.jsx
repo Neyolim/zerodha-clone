29f85fe5f5b0e8233963c8f90d5b2c2a3cb5355b
Neyolim <neyoollim@gmail.com>
2025-10-15 23:08:06 +0545

    implement Awards and Hero components with layout structure

--- frontend/src/landing_page/home/Awards.jsx
+++ frontend/src/landing_page/home/Awards.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Awards() {
     return (
-        <div>Awards</div>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-6'>
+
+                </div>
+                <div className='col-6'>
+
+                </div>
+            </div>
+
+        </div>
     )
 }
