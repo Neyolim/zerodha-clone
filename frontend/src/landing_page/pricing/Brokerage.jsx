63d17c81a649b729263ca6d13ed2fb980d7692d2
Neyolim <neyoollim@gmail.com>
2025-10-21 20:59:26 +0545

    implement pricing component

--- frontend/src/landing_page/pricing/Brokerage.jsx
+++ frontend/src/landing_page/pricing/Brokerage.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Brokerage() {
     return (
-        <div>Brokerage</div>
+        <div className='container border-top'>
+            <div className="row p-5 mt-5 text-center">
+                <div className="col-8 p-4">
+                    <a href="#" style={{ textDecoration: "none" }}><h3 className='fs-5'>Brokerage Calculator</h3></a>
+
+                    {/* For Later since styles have been changed here  */}
+                </div>
+                <div className="col-4 p-4">
+                    <a href="#" style={{ textDecoration: "none" }}><h3 className='fs-5'>List of charges</h3></a>
+                </div>
+            </div>
+        </div>
     )
 }
